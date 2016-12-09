afc3dfc098a0de1595123db7c17f072bfb52cbf5
Timothy Taylor <typicaltim@gmail.com>
2016-12-09 02:46:18 -0800

    Minor Change
    Changed the variable name from the generic testing name to something more descriptive.

--- first-yahtzee_count.py
+++ first-yahtzee_count.py
@@ -11,11 +11,11 @@ while (yahtzee != 1):
     # Increment the roll counter
     rollcounter += 1
     # Roll the die/dice
-    myarray = [random.randrange(1,7) for numberofdie in range(diecount)]
+    dieroll = [random.randrange(1,7) for numberofdie in range(diecount)]
     # Print the roll (you can make it run WAY faster if you comment out this line)
     print (myarray)
     # If it's Yahztzee, do the following:
-    if myarray[0] == myarray[1] == myarray[2] == myarray[3] == myarray[4] == myarray[0]:
+    if dieroll[0] == dieroll[1] == dieroll[2] == dieroll[3] == dieroll[4] == dieroll[0]:
         # Yay!
         print ("YAHTZEE!")
         print (rollcounter)
